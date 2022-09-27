daca252f8835281df526ac53285709fb3c7987b3
rngustj9139 <mnb9139@khu.ac.kr>
2022-09-27 23:19:30 +0900

    Updata UserController.class (RequestMapping url 변경)

--- src/main/java/koo/MicroServiceUserService/controller/UserController.java
+++ src/main/java/koo/MicroServiceUserService/controller/UserController.java
@@ -19,7 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/user-service")
+@RequestMapping("/")
 @RequiredArgsConstructor
 public class UserController {
 
